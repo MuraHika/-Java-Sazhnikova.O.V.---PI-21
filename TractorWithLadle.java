fcc21e71e966f8755133486b7e846159c9f4ede0
MuraHika <43273172+MuraHika@users.noreply.github.com>
2018-12-01 19:34:30 +0400

    Update TractorWithLadle.java

--- TractorWithLadle.java
+++ TractorWithLadle.java
@@ -37,24 +37,23 @@ public class TractorWithLadle extends Tractor {
 		int PosX = (int) _startPosX;
 		int PosY = (int) _startPosY;
 
-		int[] PointFX = new int[] { (PosX + 30), (PosX + 30), (PosX + 90) };
-		int[] PointFY = new int[] { (PosY - 20), (PosY + 50), (PosY + 50) };
+		int[] PointFX = new int[] { (PosX + 15), (PosX + 15), (PosX + 60) };
+		int[] PointFY = new int[] { (PosY - 12), (PosY + 38), (PosY + 38) };
+
 		if (Crane) {
-
 			g.setColor(MainColor);
-			g.drawRect(PosX - 50, PosY - 50, 5, 50);
-			g.fillRect(PosX - 50, PosY - 50, 5, 50);
-			g.drawRect(PosX - 45, PosY - 50, 60, 5);
-			g.fillRect(PosX - 45, PosY - 50, 60, 5);
-			g.drawRect(PosX + 15, PosY - 50, 5, 20);
-			g.fillRect(PosX + 15, PosY - 50, 5, 20);
+			g.drawRect(PosX - 50, PosY - 30, 3, 30);
+			g.fillRect(PosX - 50, PosY - 30, 3, 30);
+			g.drawRect(PosX - 47, PosY - 30, 50, 3);
+			g.fillRect(PosX - 47, PosY - 30, 50, 3);
+			g.drawRect(PosX, PosY - 30, 3, 10);
+			g.fillRect(PosX, PosY - 30, 3, 10);
 
-			g.setColor(Color.BLACK);
-			g.drawRect(PosX + 17, PosY - 30, 1, 5);
-			g.drawRect(PosX + 11, PosY - 25, 7, 1);
-			g.drawRect(PosX + 11, PosY - 30, 1, 5);
+			g.setColor(DopColor);
+			g.drawRect(PosX + 1, PosY - 20, 1, 5);
+			g.drawRect(PosX + 1, PosY - 15, 7, 1);
+			g.drawRect(PosX + 8, PosY - 20, 1, 5);
 		}
-
 		g.setColor(DopColor);
 		g.fillRect(PosX + 25, PosY + 20, 6, 10);
 		g.drawRect(PosX + 25, PosY + 20, 6, 10);
@@ -62,17 +61,17 @@ public class TractorWithLadle extends Tractor {
 		g.fillPolygon(PointFX, PointFY, 3);
 
 		g.setColor(MainColor);
-		g.fillRect(PosX - 50, PosY - 30, 50, 30);
-		g.drawRect(PosX - 50, PosY - 30, 50, 30);
-		g.fillRect(PosX - 50, PosY, 75, 40);
-		g.drawRect(PosX - 50, PosY, 75, 40);
+		g.fillRect(PosX - 50, PosY - 15, 50, 20);
+		g.drawRect(PosX - 50, PosY - 15, 50, 20);
+		g.fillRect(PosX - 50, PosY + 5, 60, 30);
+		g.drawRect(PosX - 50, PosY + 5, 60, 30);
 
 		g.setColor(GlassColor);
-		g.fillRect(PosX - 25, PosY - 25, 20, 25);
-		g.drawRect(PosX - 25, PosY - 25, 20, 25);
+		g.fillRect(PosX - 25, PosY - 12, 20, 17);
+		g.drawRect(PosX - 25, PosY - 12, 20, 17);
 
 		g.setColor(DopColor);
-		g.fillOval(PosX - 50, PosY + 10, 40, 40);
-		g.fillOval(PosX + 5, PosY + 30, 20, 20);
+		g.fillOval(PosX - 50, PosY + 10, 35, 35);
+		g.fillOval(PosX - 10, PosY + 20, 25, 25);
 	}
 }
