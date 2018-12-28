96c50cf97c86e95907bcbbfdf4b453e6b950cd6e
MuraHika <43273172+MuraHika@users.noreply.github.com>
2018-12-28 16:38:12 +0400

    Update FormGarage.java

--- FormGarage.java
+++ FormGarage.java
@@ -6,6 +6,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class FormGarage extends JFrame {
 
@@ -14,13 +16,16 @@ public class FormGarage extends JFrame {
 	private JButton buttonSetTractorWithLadle;
 	private JButton buttonTakeTractor;
 	private JPanel panel;
+	private DefaultListModel listModel;
+	private JList listLevels;
 	private JPanel panelTakeTractorFromPlace;
 	private GaragePanel panelWithGarage;
 	private DrawPanel panelTakeTractor;
 	private JLabel labelPlace;
 	private JTextField textField;
 	private ITransport transport;
-	private Garage<ITransport> garage;
+	private MultiLevelGarage garage;
+	private final int countLevel = 5;
 
 	public FormGarage() throws IOException {
 		initialize();
@@ -56,42 +61,77 @@ public class FormGarage extends JFrame {
 		panelWithGarage.setBackground(Color.lightGray);
 		panelWithGarage.setBounds(10, 11, 639, 400);
 		frame.add(panelWithGarage);
-		garage = new Garage<ITransport>(20, panelWithGarage.getWidth(), panelWithGarage.getHeight());
 		panelWithGarage.setParking(garage);
 
-		buttonSetTractor = new JButton("Поставить обычный Трактор");
+		listModel = new DefaultListModel();
+		for (int i = 0; i < countLevel; i++) {
+			listModel.addElement("Level " + Integer.toString(i + 1));
+		}
+
+		listLevels = new JList(listModel);
+		listLevels.setBounds(910, 40, 170, 124);
+		frame.add(listLevels);
+		listLevels.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		listLevels.setSelectedIndex(0);
+
+		ListSelectionListener listSelectionListener = new ListSelectionListener() {
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				panelWithGarage.repaint();
+			}
+		};
+		listLevels.addListSelectionListener(listSelectionListener);
+		garage = new MultiLevelGarage(countLevel, panelWithGarage.getWidth(), panelWithGarage.getHeight());
+		panelWithGarage.setParking(garage);
+		panelWithGarage.setList(listLevels);
+
+		buttonSetTractor = new JButton("Create Tractor");
 		buttonSetTractor.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				Color MainColor = JColorChooser.showDialog(null, "Choose a Color", Color.RED);
-				transport = new Tractor(100, 1000, MainColor);
-				garage.AddTractor(transport);
-				panelWithGarage.repaint();
+				if (listLevels.getSelectedIndex() > -1) {
+					Color MainColor = JColorChooser.showDialog(null, "Choose a Color", Color.RED);
+					transport = new Tractor(100, 1000, MainColor);
+					int place = garage.getParking(listLevels.getSelectedIndex()).AddTractor(transport);
+					if (place != -1) {
+						panelWithGarage.repaint();
+					}
+					panelWithGarage.repaint();
+				}
 			}
 		});
 		buttonSetTractor.setBounds(665, 19, 220, 60);
 		frame.add(buttonSetTractor);
 
-		buttonSetTractorWithLadle = new JButton("Поставить Трактор с ковшом");
+		buttonSetTractorWithLadle = new JButton("Create Tractor With Ladle");
 		buttonSetTractorWithLadle.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				Color MainColor = JColorChooser.showDialog(null, "Choose a Color", Color.RED);
-				Color DopColor = JColorChooser.showDialog(null, "Choose a Color", Color.BLACK);
-				Color GlassColor = JColorChooser.showDialog(null, "Choose a Color", Color.BLUE);
-				transport = new TractorWithLadle(100, 1000, MainColor, DopColor, GlassColor, true);
-				garage.AddTractor(transport);
-				panelWithGarage.repaint();
+				if (listLevels.getSelectedIndex() > -1) {
+					Color MainColor = JColorChooser.showDialog(null, "Choose a Color", Color.RED);
+					Color DopColor = JColorChooser.showDialog(null, "Choose a Color", Color.BLACK);
+					Color GlassColor = JColorChooser.showDialog(null, "Choose a Color", Color.BLUE);
+					transport = new TractorWithLadle(100, 1000, MainColor, DopColor, GlassColor, true);
+					int place = garage.getParking(listLevels.getSelectedIndex()).AddTractor(transport);
+					if (place != -1) {
+						panelWithGarage.repaint();
+					}
+					panelWithGarage.repaint();
+				}
 			}
 		});
 		buttonSetTractorWithLadle.setBounds(665, 100, 220, 60);
 		frame.add(buttonSetTractorWithLadle);
 
+		garage = new MultiLevelGarage(countLevel, panelWithGarage.getWidth(), panelWithGarage.getHeight());
+		panelWithGarage.setParking(garage);
+		panelWithGarage.setList(listLevels);
+		
 		panelTakeTractorFromPlace = new JPanel();
 		panelTakeTractorFromPlace.setBackground(Color.lightGray);
 		panelTakeTractorFromPlace.setBounds(665, 183, 170, 215);
 		frame.add(panelTakeTractorFromPlace);
 		panelTakeTractorFromPlace.setLayout(null);
 
-		labelPlace = new JLabel("Место: ");
+		labelPlace = new JLabel("Place: ");
 		labelPlace.setBounds(20, 14, 50, 14);
 		panelTakeTractorFromPlace.add(labelPlace);
 
@@ -100,9 +140,12 @@ public class FormGarage extends JFrame {
 		panelTakeTractor.setBounds(10, 72, 150, 122);
 		panelTakeTractorFromPlace.add(panelTakeTractor);
 
-		buttonTakeTractor = new JButton("Вывести из гаража ");
+		buttonTakeTractor = new JButton("Take Tractor");
 		buttonTakeTractor.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				if (listLevels.getSelectedIndex() == -1) {
+					return;
+				}
 				int numberOfPlace = 0;
 				try {
 					numberOfPlace = Integer.parseInt(textField.getText());
@@ -110,11 +153,7 @@ public class FormGarage extends JFrame {
 					textField.setText("ERROR");
 					return;
 				}
-				if (numberOfPlace >= garage._places.size() || numberOfPlace < 0) {
-					textField.setText("ERROR");
-					return;
-				}
-				transport = garage.RemoveTractor(numberOfPlace);
+				transport = garage.getParking(listLevels.getSelectedIndex()).RemoveTractor(numberOfPlace);
 				if (transport != null) {
 					transport.SetPosition(80, 50, panelTakeTractor.getWidth(), panelTakeTractor.getHeight());
 				}
@@ -130,5 +169,9 @@ public class FormGarage extends JFrame {
 		textField.setBounds(78, 11, 80, 20);
 		panelTakeTractorFromPlace.add(textField);
 		textField.setColumns(10);
+		
+		JLabel levelsLabel = new JLabel("Levels:");
+		levelsLabel.setBounds(756, 11, 56, 16);
+		panel.add(levelsLabel);
 	}
 }
