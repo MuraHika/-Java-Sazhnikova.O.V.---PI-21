f0d665d727ac342ddffc17c63c0c1730463349f6
MuraHika <43273172+MuraHika@users.noreply.github.com>
2018-12-28 16:46:19 +0400

    Add files via upload

---
+++ MultiLevelGarage.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class MultiLevelGarage {
+
+	ArrayList<Garage<ITransport>> garageStages;
+	private final int countPlaces = 20;
+
+	public MultiLevelGarage(int countStages, int pictureWidth, int pictureHeight) {
+		garageStages = new ArrayList<Garage<ITransport>>();
+		for (int i = 0; i < countStages; ++i) {
+			garageStages.add(new Garage<ITransport>(countPlaces, pictureWidth, pictureHeight));
+		}
+	}
+
+	public Garage<ITransport> getParking(int index) {
+		if ((index > -1) && (index < garageStages.size())) {
+			return garageStages.get(index);
+		}
+		return null;
+	}
+}
