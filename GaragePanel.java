441ef7de58c88731c86ecb5f9bded5756550cf97
MuraHika <43273172+MuraHika@users.noreply.github.com>
2018-12-28 16:44:47 +0400

    Update GaragePanel.java

--- GaragePanel.java
+++ GaragePanel.java
@@ -1,21 +1,28 @@
 import java.awt.Graphics;
-
+import javax.swing.JList;
 import javax.swing.JPanel;
 
 public class GaragePanel extends JPanel {
 
-	private Garage<ITransport> garage;
+	private MultiLevelGarage garage;
+	private JList listLevels;
 
-	public void setParking(Garage garage) {
+	public void setParking(MultiLevelGarage garage) {
 		this.garage = garage;
 	}
 
+	public void setList(JList listLevels) {
+		this.listLevels = listLevels;
+	}
+	
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
 		try {
 			if (garage != null) {
-				garage.Draw(g);
+				if (listLevels.getSelectedIndex() != -1) {
+					garage.getParking(listLevels.getSelectedIndex()).Draw(g);
+				}
 			}
 		} catch (Exception ex) {
 		}
