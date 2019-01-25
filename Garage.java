fd96395ac5966e0d9447eaeeca1f9d2f1fe511ae
MuraHika <43273172+MuraHika@users.noreply.github.com>
2019-01-25 16:27:35 +0400

    Update Garage.java

--- Garage.java
+++ Garage.java
@@ -3,7 +3,7 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Garage<T extends ITransport> {
+public class Garage<T extends ITransport> implements Serializable, Comparable<Garage<T>>, Iterable<T>, Iterator<T> {
 
 	private int _placeSizeWidth = 250;
 	private int _placeSizeHeight = 80;
@@ -11,6 +11,7 @@ public class Garage<T extends ITransport> {
 	private int ScreenHeight;
 	private HashMap<Integer, T> _places;
 	private int _maxCount;
+	private int currentIndex;
 
 	public int getPictureWidth() {
 		return ScreenWidth;
@@ -35,17 +36,28 @@ public class Garage<T extends ITransport> {
 		this.ScreenHeight = pictureHeight;
 	}
 
-	public int AddTractor(T transport) throws GarageOverflowException, GarageOccupiedPlaceException {
+	public int AddTractor(T transport) throws GarageOverflowException, GarageOccupiedPlaceException, GarageAlreadyHaveException {
 		if (_places.size() == _maxCount) {
 			throw new GarageOverflowException();
 		}
-		for (int i = 0; i < _maxCount; i++) {
+		int index = _places.size();
+		for (int i = 0; i <= _places.size(); i++) {
 			if (checkFreePlace(i)) {
-				_places.add(i, transport);
-				_places.get(i).SetPosition(5 + i / 5 * _placeSizeWidth + 5 + 50, i % 5 * _placeSizeHeight + 35,
-						ScreenWidth, ScreenHeight);
-				return i;
+				index = i;
 			}
+			if (_places.containsValue(transport)) {
+				throw new GarageAlreadyHaveException();
+			}
+			if (index != _places.size()) {
+				_places.put(index, transport);
+				_places.get(index).SetPosition(5 + i / 5 * _placeSizeWidth + 5 + 50, i % 5 * _placeSizeHeight + 35,
+						ScreenWidth, ScreenHeight);
+				return index;
+			}
+			this._places.put(this._places.size(), transport);
+			_places.get(index).SetPosition(5 + index / 5 * _placeSizeWidth + 5 + 50, index % 5 * _placeSizeHeight + 35,
+					ScreenWidth, ScreenHeight);
+			return this._places.size() - 1;
 		}
 		throw new GarageOccupiedPlaceException();
 	}
@@ -98,5 +110,62 @@ public class Garage<T extends ITransport> {
 			_places.get(ind).SetPosition(5 + ind / 5 * _placeSizeWidth + 5 + 50, ind % 5 * _placeSizeHeight + 35,
 					ScreenWidth, ScreenHeight);
 		}
+	}
+	
+	@Override
+	public int compareTo(Garage<T> other) {
+		if (this._places.size() > other._places.size()) {
+			return -1;
+		} else if (this._places.size() < other._places.size()) {
+			return 1;
+		} else {
+			Integer[] thisKeys = this._places.keySet().toArray(new Integer[this._places.size()]);
+			Integer[] otherKeys = other._places.keySet().toArray(new Integer[other._places.size()]);
+			for (int i = 0; i < this._places.size(); i++) {
+				if (this._places.get(thisKeys[i]).getClass().equals(Tractor.class)
+						&& other._places.get(otherKeys[i]).getClass().equals(TractorWithLadle.class)) {
+					return 1;
+				}
+				if (this._places.get(thisKeys[i]).getClass().equals(TractorWithLadle.class)
+						&& other._places.get(otherKeys[i]).getClass().equals(Tractor.class)) {
+					return -1;
+				}
+				if (this._places.get(thisKeys[i]).getClass().equals(Tractor.class)
+						&& other._places.get(otherKeys[i]).getClass().equals(Tractor.class)) {
+					return ((Tractor) this._places.get(thisKeys[i]))
+							.compareTo((Tractor) other._places.get(otherKeys[i]));
+				}
+				if (this._places.get(thisKeys[i]).getClass().equals(TractorWithLadle.class)
+						&& other._places.get(otherKeys[i]).getClass().equals(TractorWithLadle.class)) {
+					return ((TractorWithLadle) this._places.get(thisKeys[i]))
+							.compareTo((TractorWithLadle) other._places.get(otherKeys[i]));
+				}
+			}
+		}
+		return 0;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return this;
+	}
+
+	@Override
+	public boolean hasNext() {
+		if (currentIndex + 1 >= _places.size()) {
+			currentIndex = -1;
+			return false;
+		}
+		currentIndex++;
+		return true;
+	}
+
+	@Override
+	public T next() {
+		return (T) _places.get(currentIndex);
+	}
+
+	private void reset() {
+		currentIndex = -1;
 	}
 }
